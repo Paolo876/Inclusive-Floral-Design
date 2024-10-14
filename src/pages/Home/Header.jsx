848229cb02b62233b42994578fcda23d4df1dc30
paoloBugarin <paolopaolobugarin@gmail.com>
2024-10-13 18:07:00 -0700

    updated home paGE

--- src/pages/Home/Header.jsx
+++ src/pages/Home/Header.jsx
@@ -1,13 +1,74 @@
 import React from 'react'
 import bg from "../../assets/header_bg.avif"
 import Image from 'mui-image'
-import { Box } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
+import bannerImg from "../../assets/ifd_banner.png"
+
 
 const Header = () => {
   return (
-    <Box>
-      <Box sx={{height: "40em"}}>
-        <Image src={bg} alt="header-bg" fit='cover'/>
+    <Box sx={{
+      position: "relative",
+    }}>
+      {/* background */}
+      <Box sx={{height: "40em", }}>
+        <Image src={bg} alt="header-bg" fit='cover' sx={{filter: "brightness(90%) contrast(92%)"}}/>
+      </Box>
+      {/* message */}
+      <Box
+        sx={{
+          position:"absolute",
+          left: "50%",
+          top: "45%",
+          transform: "translate(-50%, -50%)",
+          px: 7,
+          py: 4,
+          "&::before": {
+            content: '""',
+            top: 0,
+            left: 0,
+            backdropFilter: "blur(2px) contrast(10%) brightness(180%) opacity(85%)",
+            position: "absolute",
+            height: "100%",
+            width: "100%",
+            zIndex: -1
+          },
+          "&::after": {
+            content: '""',
+            top: "50%",
+            left: "50%",
+            border: 1,
+            borderColor: "white",
+            position: "absolute",
+            height: "105%",
+            width: "102.5%",
+            transform: "translate(-50%, -50%)",
+            zIndex: -1
+          } 
+        }}
+      >
+        <Image src={bannerImg} alt="banner-img" fit="scale-down" sx={{pointerEvents: "none"}}/>
+        <Box
+          sx={{
+            mx: "auto",
+            display: "flex",
+            mt: 3
+          }}
+        >
+          <Button 
+            variant="contained"
+            color='secondary'
+            size="large"
+            sx={{
+              mx: "auto",
+              borderRadius: 0,
+              letterSpacing: 1.8,
+              px: 5
+            }}
+          >
+            Shop Now
+          </Button>
+        </Box>
       </Box>
     </Box>
   )
